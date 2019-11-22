4e6c983800e03d123694a789a56d147cba761528
Ryan <drdgvhbh@gmail.com>
2019-11-22 15:23:37 -0500

    It works

--- src/Leecher.tsx
+++ src/Leecher.tsx
@@ -5,6 +5,11 @@ import nanoid from "nanoid";
 import * as config from "./config";
 import Matic from "maticjs";
 import BigNumber from "bignumber.js";
+import Web3 from "web3";
+
+const web3 = new Web3();
+
+// (window as any).web3.currentProvider
 
 const matic = new Matic({
   maticProvider: config.MATIC_PROVIDER,
@@ -15,13 +20,23 @@ const matic = new Matic({
   maticWethAddress: config.MATICWETH_ADDRESS
 });
 
-console.log(matic)
+console.log(matic);
 const token = config.ROPSTEN_TEST_TOKEN;
 
 const ONE_GWEI = new BigNumber("00000000001");
-(matic.wallet as any) = "3f37a3de5db9860e600d578363534cf0f31814b5e6a2253f627e1802a2c35f9e";
+(matic.wallet as any) = "0x3f37a3de5db9860e600d578363534cf0f31814b5e6a2253f627e1802a2c35f9e";
 
-const PUBLIC_KEY = "0x8331E67aBd1D5248e349bc86E032Fd7bf4C7008d";
+const PUBLIC_KEY = "0x8331e67abd1d5248e349bc86e032fd7bf4c7008d";
+// 0x8331E67aBd1D5248e349bc86E032Fd7bf4C7008d
+
+// 3F37A3DE5DB9860E600D578363534CF0F31814B5E6A2253F627E1802A2C35F9E
+
+interface DownloadState {
+  lastDownloaded: number;
+  totalDownloaded: number;
+  downloadSpeed: number;
+  progress: number;
+}
 
 const Leecher = () => {
   const [client, setClient] = useState(new WebTorrent());
@@ -29,12 +44,73 @@ const Leecher = () => {
   const [magnetURI, setMagnetURI] = useState("");
   const [seederAddr, setSeederAddr] = useState("");
 
+  const [initialDeposit, setInitialDeposit] = useState("0");
+  const [downloadState, setDownloadState] = useState<DownloadState>({
+    lastDownloaded: 0,
+    totalDownloaded: 0,
+    downloadSpeed: 0,
+    progress: 0
+  });
+  const [tokensInMatic, setTokensInMatic] = useState("");
+
+  setTimeout(async () => {
+    const balance = await matic.balanceOfERC20(
+      PUBLIC_KEY,
+      "0xc82c13004c06E4c627cF2518612A55CE7a3Db699",
+      {}
+    );
+
+    console.log(balance);
+    setTokensInMatic(balance);
+  }, 1000);
+
   return (
     <div>
+      <div>
+        <div>1000000000000000000</div>
+        <h2>Initial Deposit</h2>
+        <input
+          value={initialDeposit}
+          onChange={value => setInitialDeposit(value.target.value)}
+        />
+        <button
+          onClick={() => {
+            matic
+              .approveERC20TokensForDeposit(token, `${initialDeposit}`, {
+                from: PUBLIC_KEY,
+                onTransactionHash: hash => {
+                  // action on Transaction success
+                  console.log(hash); // eslint-disable-line
+                }
+              })
+              .then(() => {
+                matic.depositERC20Tokens(
+                  token,
+                  PUBLIC_KEY,
+                  `${initialDeposit}`,
+                  {
+                    from: PUBLIC_KEY,
+                    onTransactionHash: hash => {
+                      // action on Transaction success
+                      console.log(hash); // eslint-disable-line
+                    }
+                  }
+                );
+              });
+          }}
+        >
+          Submit Initial Deposit
+        </button>
+      </div>
+      <div></div>
+
+      <h2>Magnet URI</h2>
       <input
         value={magnetURI}
         onChange={value => setMagnetURI(value.target.value)}
       />
+
+      <h2>Seeder Public Key</h2>
       <input
         value={seederAddr}
         onChange={value => setSeederAddr(value.target.value)}
@@ -47,27 +123,27 @@ const Leecher = () => {
               const amount = `${new BigNumber(torrent.downloaded).multipliedBy(
                 ONE_GWEI
               )}`;
-              matic
-                .approveERC20TokensForDeposit(token, `${amount}`, {
-                  from: PUBLIC_KEY,
-                  onTransactionHash: hash => {
-                    // action on Transaction success
-                    console.log(hash); // eslint-disable-line
-                  }
-                })
-                .then(() => {
-                  matic.depositERC20Tokens(token, PUBLIC_KEY, `${amount}`, {
-                    from: PUBLIC_KEY,
-                    onTransactionHash: hash => {
-                      // action on Transaction success
-                      console.log(hash); // eslint-disable-line
-                    }
-                  });
-                });
-              console.log("just downloaded: " + bytes);
+
+              setDownloadState({
+                lastDownloaded: bytes,
+                totalDownloaded: torrent.downloaded,
+                downloadSpeed: torrent.downloadSpeed,
+                progress: torrent.progress
+              });
+
+              await matic.transferTokens(
+                "0xc82c13004c06E4c627cF2518612A55CE7a3Db699",
+                seederAddr,
+                amount,
+                {
+                  from: PUBLIC_KEY
+                }
+              );
+
+              /*            console.log("just downloaded: " + bytes);
               console.log("total downloaded: " + torrent.downloaded);
               console.log("download speed: " + torrent.downloadSpeed);
-              console.log("download progress: " + torrent.progress);
+              console.log("download progress: " + torrent.progress); */
             });
           });
           client.add(magnetURI, function(torrent) {
@@ -83,8 +159,34 @@ const Leecher = () => {
           });
         }}
       >
-        GOGO
+        Start Downloading
       </button>
+      <div></div>
+      <div>
+        <span>Tokens in Channel: </span>
+        {tokensInMatic || "0"}
+        {" TEST"}
+      </div>
+      <div>
+        <span>Last Download: </span>
+        {downloadState.lastDownloaded}
+        {`bytes`}
+      </div>
+      <div>
+        <span>Total Download: </span>
+        {downloadState.totalDownloaded}
+        {`bytes`}
+      </div>
+      <div>
+        <span>Download Speed: </span>
+        {downloadState.downloadSpeed}
+        {`bytes`}
+      </div>
+      <div>
+        <span>Progress: </span>
+        {downloadState.progress}
+        {`bytes`}
+      </div>
     </div>
   );
 };
--- src/Seeder.tsx
+++ src/Seeder.tsx
@@ -7,18 +7,30 @@ import * as config from "./config";
 import Matic from "maticjs";
 import { BigNumber } from "bignumber.js";
 
+interface UploadState {
+  lastUploaded: number;
+  totalUploaded: number;
+  uploadSpeed: number;
+  progress: number;
+}
+
+// import Web3 from "web3";
+
+// const web3 = new Web3(window.web3.currentProvider);
 let MagnetURI = "";
 
+// console.log(web3.currentProvider);
 const matic = new Matic({
   maticProvider: config.MATIC_PROVIDER,
   parentProvider: config.PARENT_PROVIDER,
+  //parentProvider: (window as any).web3.currentProvider,
   rootChainAddress: config.ROOTCHAIN_ADDRESS,
   syncerUrl: config.SYNCER_URL,
   watcherUrl: config.WATCHER_URL,
   maticWethAddress: config.MATICWETH_ADDRESS
 });
-(matic.wallet as any) = "e7f96af2a28403f3fc3c76ab3a425fdb9720e63f391e694c10bb01bd0937bfdc";
-console.log(matic)
+(matic.wallet as any) = "0xe7f96af2a28403f3fc3c76ab3a425fdb9720e63f391e694c10bb01bd0937bfdc";
+console.log(matic);
 
 const ONE_GWEI = new BigNumber("00000000001");
 
@@ -29,13 +41,25 @@ const PUBLIC_KEY = "0xb41C61744490440c024989AB4F11289911A39Fcc";
 const Seeder = () => {
   const [client] = useState(new WebTorrent());
 
+  const [uploadState, setUploadState] = useState<UploadState>({
+    lastUploaded: 0,
+    totalUploaded: 0,
+    uploadSpeed: 0,
+    progress: 0
+  });
+  const [tokensInMatic, setTokensInMatic] = useState("");
+
   const [infoHash, setInfoHash] = useState("");
   const [magnetURI, setMagnetURI] = useState<string>("");
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const balance = await matic.web3.eth.getBalance(token);
+      const balance = await matic.balanceOfERC20(
+        PUBLIC_KEY,
+        "0xc82c13004c06E4c627cF2518612A55CE7a3Db699"
+      );
       console.log(balance);
+      setTokensInMatic(balance);
     }, 1000);
     return () => clearTimeout(timer);
   }, []);
@@ -61,10 +85,12 @@ const Seeder = () => {
           MagnetURI = torrent.magnetURI;
 
           torrent.on("upload", bytes => {
-            console.log("just uploaded: " + bytes);
-            console.log("total uploaded: " + torrent.uploaded);
-            console.log("upload speed: " + torrent.uploadSpeed);
-            console.log("upload progress: " + torrent.progress);
+            setUploadState({
+              uploadSpeed: torrent.uploadSpeed,
+              totalUploaded: torrent.uploaded,
+              lastUploaded: bytes,
+              progress: torrent.progress
+            });
           });
         });
       };
@@ -95,6 +121,38 @@ const Seeder = () => {
         </div>
       )}
       {magnetURI && <QRCode size={300} value={magnetURI} />}
+      <div>
+        <div></div>
+        <div>
+          {`Public Key: `}
+          {PUBLIC_KEY}
+        </div>
+        <div>
+          <span>Tokens in Channel: </span>
+          {tokensInMatic || "0"}
+          {" TEST"}
+        </div>
+        <div>
+          <span>Last Upload: </span>
+          {uploadState.lastUploaded}
+          {`bytes`}
+        </div>
+        <div>
+          <span>Total Upload: </span>
+          {uploadState.totalUploaded}
+          {`bytes`}
+        </div>
+        <div>
+          <span>Upload Speed: </span>
+          {uploadState.uploadSpeed}
+          {`bytes`}
+        </div>
+        <div>
+          <span>Progress: </span>
+          {uploadState.progress}
+          {`bytes`}
+        </div>
+      </div>
     </div>
   );
 };
